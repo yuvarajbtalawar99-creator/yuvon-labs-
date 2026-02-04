fb011c171e45f92716e51c2f01f37fc64729d334
Yuvaraj B Talawar <yuvarajbtalawar99@gmail.com>
2026-02-05 00:48:48 +0530

    feat: Elite Polish Phase 4 (Smooth Kinetic Scrolling with Lenis)

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Index from "./pages/Index";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsOfService from "./pages/TermsOfService";
 import NotFound from "./pages/NotFound";
+import SmoothScroll from "./components/SmoothScroll";
 
 const queryClient = new QueryClient();
 
@@ -15,15 +16,17 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route path="/terms-of-service" element={<TermsOfService />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <SmoothScroll>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/terms-of-service" element={<TermsOfService />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </SmoothScroll>
     </TooltipProvider>
   </QueryClientProvider>
 );
---
+++ src/components/SmoothScroll.tsx
@@ -0,0 +1,53 @@
+import { useEffect } from 'react';
+import Lenis from 'lenis';
+
+interface SmoothScrollProps {
+    children: React.ReactNode;
+}
+
+const SmoothScroll = ({ children }: SmoothScrollProps) => {
+    useEffect(() => {
+        const lenis = new Lenis({
+            duration: 1.2,
+            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+            orientation: 'vertical',
+            gestureOrientation: 'vertical',
+            smoothWheel: true,
+            wheelMultiplier: 1,
+            touchMultiplier: 2,
+            infinite: false,
+        });
+
+        function raf(time: number) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        }
+
+        requestAnimationFrame(raf);
+
+        // Handle internal anchor links
+        const handleAnchorClick = (e: MouseEvent) => {
+            const target = e.target as HTMLElement;
+            const anchor = target.closest('a');
+
+            if (anchor && anchor.hash && anchor.origin === window.location.origin) {
+                e.preventDefault();
+                const targetElement = document.querySelector(anchor.hash);
+                if (targetElement) {
+                    lenis.scrollTo(targetElement as HTMLElement);
+                }
+            }
+        };
+
+        document.addEventListener('click', handleAnchorClick);
+
+        return () => {
+            lenis.destroy();
+            document.removeEventListener('click', handleAnchorClick);
+        };
+    }, []);
+
+    return <>{children}</>;
+};
+
+export default SmoothScroll;
